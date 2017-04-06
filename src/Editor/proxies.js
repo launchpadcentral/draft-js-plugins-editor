69f88ade4023423a028b8a7793773769f8ff7d26
Andrew Kahn <andrew@andrewkahn.me>
2017-04-06 15:22:10 -0700

    idkv7

--- src/Editor/proxies.js
+++ src/Editor/proxies.js
@@ -1,7 +1,6 @@
 // the list of available proxies can be found here: https://github.com/facebook/draft-js/blob/master/src/component/base/DraftEditor.react.js#L120
 const proxies = [
-  'focus',
-  'blur'
+  'focus'
 ];
 
 export default proxies;
